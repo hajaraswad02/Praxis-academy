384ad0bd696e6e654ea2371595a71e0a326672f1
Justin <ibnuaswat@gmail.com>
2020-10-13 17:08:06 +0700

    Boot Camp

---
+++ novice/02-02/Plat Nomor (Int).js
@@ -0,0 +1,10 @@
+function ValidatePNM(PNomor)
+{
+	if (/^[a-zA-z]{1,2}\s?\d{1,4}\s?[a-zA-Z]{1,3}$/.test(My.ValidatePNM.value))
+
+}
+return (true)
+{
+	alert("Nomor Plat kamu sudah betul")
+    return (false)
+}
